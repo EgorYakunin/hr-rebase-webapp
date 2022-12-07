afd64d561a1872a88277478dc62675de56567f30
EgorYakunin <e.yakunin04@gmail.com>
2022-12-07 10:43:36 +0300

    changed faq

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,3 +1,13 @@
+import Container from '@/components/std/Container';
+import Spacer from '@/components/std/Spacer';
+
+import styles from './faq.module.css';
+
 export default function FaqPage() {
-    return <h1>This is faq page</h1>;
+    return (
+        <Container>
+            <h1>This is faq page </h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem omnis minus, numquam dolor recusandae alias quibusdam quam eveniet libero earum!</p>
+        </Container>
+    );
 }
